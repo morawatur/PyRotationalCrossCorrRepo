e8a3648c314234201ad37bd8fb219e143fd0640f
moraw <krzymoraw@gmail.com>
2016-12-16 15:55:23 +0100

    Adjusted main script for RotationalCrossCorr program

--- Main.py
+++ Main.py
@@ -1,9 +1,15 @@
 from numba import cuda
-import Constants as const
-import GUI as gui
+import numpy as np
+import Dm3Reader3 as dm3
+import ImageSupport as imsup
 
 cuda.select_device(0)
 dev = cuda.get_current_device()
 print('CUDA device in use: ' + dev.name.decode())
 
-gui.RunEwrWindow(const.gridDim)
+imgData = dm3.ReadDm3File('img1.dm3')
+imgMatrix = imsup.PrepareImageMatrix(imgData, 1024)
+img = imsup.Image(1024,1024, imsup.Image.cmp['CAP'], imsup.Image.mem['CPU'])
+img.amPh.am = np.sqrt(imgMatrix)
+
+imsup.RotateImage(img, 137)
